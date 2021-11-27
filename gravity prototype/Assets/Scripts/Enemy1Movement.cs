9eb8c1213d6708031de19c81f73173eadbc1d860
Etienne Pham Do <etphamdo@gmail.com>
2021-11-27 16:21:04 -0500

    a pretty big commit
    -Navmesh for tilemap added
    -AI movement for enemy
    -New bullet asset

--- gravity prototype/Assets/Scripts/Enemy1Movement.cs
+++ gravity prototype/Assets/Scripts/Enemy1Movement.cs
@@ -1,39 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Enemy1Movement : MonoBehaviour
 {
     public static float characterVelocity = 1f;
     private Vector2 movementDirection;
     private Vector2 movementPerSecond;
     public Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
+    public NavMeshAgent enemy_ai;
+    private bool isFlipped = true;
+    private SpriteRenderer player;
+    
+    private void Start()
     {
         movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), 0).normalized;
-        //movementPerSecond = movementDirection * characterVelocity;
+        enemy_ai.updateRotation = false;
+        enemy_ai.updateUpAxis = false;
+        player = GameObject.Find("player").GetComponent<SpriteRenderer>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        movementPerSecond = movementDirection * characterVelocity;
-        //move enemy: 
-        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y);
+        float distance = transform.position.magnitude - GameObject.Find("potato fight").transform.position.magnitude;
+        if(Mathf.Abs(distance) < 3)
+        {
+            enemy_ai.enabled = true;
+            enemy_ai.SetDestination(GameObject.Find("potato fight").transform.position);
+            if (player.flipX)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+        }
+        else
+        {
+            enemy_ai.enabled = false;
+            movementPerSecond = movementDirection * characterVelocity;
+            //move enemy: 
+            transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime), transform.position.y);
 
-        // For the movement animations
-        animator.SetFloat("Horizontal", movementDirection.x);
-        //animator.SetFloat("Vertical", movementDirection.y);
-        animator.SetFloat("Speed", movementPerSecond.magnitude);
+            // For the movement animations
+            animator.SetFloat("Speed", movementPerSecond.magnitude);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Stop")
         {
-            movementDirection.x *= -1;
+            movementDirection *= -1;
+            if(movementDirection.x < 0)
+            {
+                isFlipped = false;
+            }
+            else
+            {
+                isFlipped = true;
+            }
+            GetComponent<SpriteRenderer>().flipX = isFlipped;
         }
     }
 }
